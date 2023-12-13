26c97d818d4efe34827203f1356d955bfec73567
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-12 21:07:33 -0300

    :construction: In progress a logic to tasks

--- src/js/tasks/UpdateTasks.js
+++ src/js/tasks/UpdateTasks.js
@@ -1,40 +1,50 @@
-export class Task {
-  constructor(root) {
-    this.root = document.querySelector(root);
-  }
-}
-
-export class TaskView extends Task {
-  constructor(root) {
-    super(root);
-  }
-
-  async add() {
-    this.div = document.querySelector(".container-task");
-    console.log(this.div);
+export class TaskView {
+  taskList = [];
+  add() {
     const addTask = document.querySelector(".add-task");
-    if (addTask) {
-      console.log(document.querySelector(".add-task"));
 
+    if (addTask) {
       addTask.addEventListener("click", () => {
-        console.log("add task");
-        const taskElement = this.createTask();
-        console.log(taskElement);
-        this.div.append(taskElement);
+        const taskData = {
+          name: "task 1",
+        };
+
+        this.updateTask(taskData);
+        this.save();
       });
     }
   }
 
-  createTask() {
+  updateTask(taskData) {
+    this.taskList.push(taskData);
+
     const div = document.createElement("div");
     div.classList.add("task-list");
-    console.log("create task");
 
     div.innerHTML = `
-            <h2>task 1</h2>
-            <button>x</button>
+            <h2>${taskData.name}</h2>
+            <button class="remove-task">x</button>
         `;
 
-    return div;
+    div.querySelector(".remove-task").addEventListener("click", () => {
+      this.delete(div, taskData);
+    });
+
+    console.log(this.root);
+    console.log(div);
+    this.div.append(div);
+  }
+
+  delete(taskElement, taskData) {
+    taskElement.remove();
+  }
+
+  save() {
+    localStorage.setItem("tasks", JSON.stringify(this.taskList));
+    console.log(JSON.parse(localStorage.getItem("tasks")));
+  }
+
+  load() {
+    JSON.parse(localStorage.getItem("tasks"));
   }
 }
